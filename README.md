cb61892b15a2a930b970f9dab20fd315f0796bc9
alen-alex <zzjj0818@gmail.com>
2015-04-15 12:14:50 +0800

    version 0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+Goagent IP Updater
+======================================
+## About
+| Author: alen-alex
+| HomePage: https://github.com/alen-alex/goagent-ip-updater
+| Last update date: 2015-04-15
+| Latest version: 0.1
+
+## Documents
+|    |   |
+| --------   | :----  |
+| Simple Guide | https://github.com/alen-alex/goagent-ip-updater/SimpleGuide.md |
+| History | https://github.com/alen-alex/goagent-ip-updater/History.md |
+
+## Code
+| | |
+| --------   | :----  |
+| search_ip.py | https://github.com/alen-alex/goagent-ip-updater |
+| insert_iplist.py | https://github.com/alen-alex/goagent-ip-updater |
+| search_ip.exe | https://github.com/alen-alex/goagent-ip-updater/dist |
+
+## License
+ * [GNU GPL v2](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
---
+++ insert_iplist.py
@@ -0,0 +1,30 @@
+# -*- coding:utf8 -*-
+
+def get_iplist(ip_file):
+    with open(ip_file,"rb") as f:
+        lines = iter(f)
+        return next(lines)
+
+def read_file(ini_file,iplist):
+    ini_content = []
+    with open(ini_file,"rb") as f:
+        for line in f:
+            if line.startswith("google_cn") or line.startswith("google_hk"):
+                line = line[:12]+iplist+"\r\n"
+            ini_content.append(line)
+        return ini_content
+
+def write_file(ini_file,ini_content):
+    with open(ini_file,"wb") as f:
+        for line in ini_content:
+            f.write(line)
+
+def update_ini():
+    ip_file = "output.txt"
+    ini_file = "local/proxy.ini"
+    iplist = get_iplist(ip_file)
+    new_content = read_file(ini_file,iplist)
+    write_file(ini_file,new_content)
+
+if __name__ == '__main__':
+    update_ini()
